1f4f270bdc462e905519d8e7cdaf3e7cb9865fcf
Paweł Ciszewski <kardahim@gmail.com>
2022-10-14 16:31:19 +0200

    add ReservationPage with select time logic

--- src/pages/reservation/Reservation.tsx
+++ src/pages/reservation/Reservation.tsx
@@ -1,4 +1,4 @@
-import { Paper, Stack } from '@mui/material'
+import { Button, Container, Paper, Stack, Box } from '@mui/material'
 import React from 'react'
 import './Reservation.scss'
 // date
@@ -8,9 +8,13 @@ import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import dayjs, { Dayjs } from 'dayjs';
 import TextField from '@mui/material/TextField';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import PersonIcon from '@mui/icons-material/Person';
+// assets
+import map from '../../assets/images/placeholder.png'
 
 
 function Reservation() {
+    // pickers hooks
     const [value, setValue] = React.useState<Dayjs | null>(
         dayjs()
     );
@@ -33,38 +37,147 @@ function Reservation() {
         }
     }, [])
 
+    // tables hooks
+    // example data
+    const [tables, setTables] = React.useState([
+        {
+            name: "Stolik nr 1",
+            slots: 3,
+            isfree: true
+        },
+        {
+            name: "Stolik nr 2",
+            slots: 2,
+            isfree: false
+        },
+        {
+            name: "Stolik nr 3",
+            slots: 1,
+            isfree: true
+        },
+        {
+            name: "Stolik nr 4",
+            slots: 5,
+            isfree: true
+        },
+        {
+            name: "Stolik nr 1",
+            slots: 3,
+            isfree: true
+        },
+        {
+            name: "Stolik nr 2",
+            slots: 2,
+            isfree: false
+        },
+        {
+            name: "Stolik nr 3",
+            slots: 1,
+            isfree: true
+        },
+        {
+            name: "Stolik nr 4",
+            slots: 5,
+            isfree: true
+        },
+        {
+            name: "Stolik nr 1",
+            slots: 3,
+            isfree: true
+        },
+        {
+            name: "Stolik nr 2",
+            slots: 2,
+            isfree: false
+        },
+        {
+            name: "Stolik nr 3",
+            slots: 1,
+            isfree: true
+        },
+        {
+            name: "Stolik nr 4",
+            slots: 5,
+            isfree: true
+        },
+        {
+            name: "Stolik nr 1",
+            slots: 3,
+            isfree: true
+        },
+        {
+            name: "Stolik nr 2",
+            slots: 2,
+            isfree: false
+        },
+        {
+            name: "Stolik nr 3",
+            slots: 1,
+            isfree: true
+        },
+        {
+            name: "Stolik nr 4",
+            slots: 5,
+            isfree: true
+        }
+    ])
+
     return (
-        <Paper elevation={2} className='reservation reservation--padding'>
-            <div className='reservation__data'>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <Stack
-                        spacing={3}
-                        direction='row'
-                        justifyContent="center"
-                        alignItems="center">
-                        <DesktopDatePicker
-                            label="Data"
-                            inputFormat='DD.MM.YYYY'
-                            value={value}
-                            onChange={handleChange}
-                            renderInput={(params) => <TextField {...params} />}
-                            disablePast
-                        />
-                        <TimePicker
-                            label="Godzina"
-                            value={value}
-                            onChange={handleChange}
-                            renderInput={(params) => <TextField {...params} />}
-                            ampm={false}
-                            minTime={minTime}
-                            maxTime={dayjs().hour(18).minute(0)}
-                        />
+        <Container maxWidth="lg">
+            <Paper elevation={2} className='reservation'>
+                <Box className='reservation__header'>
+                    rezerwacja stolika
+                </Box>
+                <div className='reservation__data'>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <Stack
+                            spacing={3}
+                            direction={{ xs: 'column', sm: 'row' }}
+                            justifyContent="center"
+                            alignItems="center">
+                            {/* add onChange */}
+                            <DesktopDatePicker
+                                label="Data"
+                                inputFormat='DD.MM.YYYY'
+                                value={value}
+                                onChange={handleChange}
+                                renderInput={(params) => <TextField {...params} />}
+                                disablePast
+                            />
+                            <TimePicker
+                                label="Godzina"
+                                value={value}
+                                onChange={handleChange}
+                                renderInput={(params) => <TextField {...params} />}
+                                ampm={false}
+                                minTime={minTime}
+                                maxTime={dayjs().hour(18).minute(0)}
+                            />
+                        </Stack>
+                    </LocalizationProvider>
+                </div>
+                <div className='reservation__table-list'>
+                    <Stack spacing={2}>
+                        {/* divider */}
+                        {tables.map((value, key) =>
+                            <div key={key} className='reservation__table-list__item'>
+                                <div>{value.name}<PersonIcon sx={{ margin: '0 0  0 20px' }} />x{value.slots}</div>
+                                <Button className='reservation__button'
+                                    variant='contained'
+                                    type='submit'
+                                >Złóż zamówienie</Button>
+                            </div>
+                        )}
                     </Stack>
-                </LocalizationProvider>
-            </div>
-            <div className='reservation__table-list'></div>
-            <div className='reservation__map'></div>
-        </Paper >
+                </div>
+                <div className='reservation__map'>
+                    <Box className='map__header'>
+                        mapa sali
+                    </Box>
+                    <img src={map} alt='map' />
+                </div>
+            </Paper >
+        </Container>
     )
 }
 
