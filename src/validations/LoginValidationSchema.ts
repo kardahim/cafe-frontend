f12f616958b9b7b48dfa7da4b12c598e1841224e
Paweł Ciszewski <kardahim@gmail.com>
2022-10-14 11:50:48 +0200

    check correct email in LoginValidationSchema

--- src/validations/LoginValidationSchema.ts
+++ src/validations/LoginValidationSchema.ts
@@ -3,7 +3,8 @@ import * as Yup from "yup"
 export const LoginValidationSchema = Yup.object({
     email: Yup
         .string()
-        .required("Email jest wymagany"),
+        .required("Email jest wymagany")
+        .email("Wprowadź poprawny adres email"),
     password: Yup
         .string()
         .required("Hasło jest wymagane")
