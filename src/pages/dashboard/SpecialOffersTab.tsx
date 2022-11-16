e193c0d530d735d889a4417e5fdcd00955f85563
Paweł Ciszewski <kardahim@gmail.com>
2022-11-16 14:38:44 +0100

    off specialOffer edit

--- src/pages/dashboard/SpecialOffersTab.tsx
+++ src/pages/dashboard/SpecialOffersTab.tsx
@@ -29,7 +29,7 @@ function SpecialOffersTab(props: SpecialOffersTabInterface) {
             type: 'number',
             align: 'center',
             headerAlign: 'center',
-            editable: true,
+            // editable: true,
             preProcessEditCellProps: (params: GridPreProcessEditCellProps) => {
                 const hasError = params.props.value < 5 || params.props.value > 100
                 return { ...params.props, error: hasError };
@@ -43,7 +43,7 @@ function SpecialOffersTab(props: SpecialOffersTabInterface) {
             type: 'date',
             align: 'center',
             headerAlign: 'center',
-            editable: true,
+            // editable: true,
             renderCell: params => dayjs(params.row.start_date).format('DD.MM.YYYY'),
             preProcessEditCellProps: (params: GridPreProcessEditCellProps) => {
                 const hasError = (dayjs(params.props.value).add(1, 'day')).isBefore(dayjs()) || (dayjs(params.props.value).add(1, 'day')).isAfter(params.row.end_date)
@@ -57,7 +57,7 @@ function SpecialOffersTab(props: SpecialOffersTabInterface) {
             type: 'date',
             align: 'center',
             headerAlign: 'center',
-            editable: true,
+            // editable: true,
             renderCell: params => dayjs(params.row.end_date).format('DD.MM.YYYY'),
             preProcessEditCellProps: (params: GridPreProcessEditCellProps) => {
                 const hasError = (dayjs(params.props.value).subtract(1, 'day')).isBefore(dayjs(params.row.start_date))
@@ -75,7 +75,7 @@ function SpecialOffersTab(props: SpecialOffersTabInterface) {
             })),
             align: 'center',
             headerAlign: 'center',
-            editable: true,
+            // editable: true,
             renderCell: params => {
                 const product = props.products.find((product) => product.id == params.row.ProductId)
                 return (product.name)
