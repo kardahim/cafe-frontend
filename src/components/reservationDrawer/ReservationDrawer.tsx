9be69ce6774c73158a31498d10df2343478c87c0
Paweł Ciszewski <kardahim@gmail.com>
2022-11-30 21:44:34 +0100

    add active reservations

--- src/components/reservationDrawer/ReservationDrawer.tsx
+++ src/components/reservationDrawer/ReservationDrawer.tsx
@@ -1,9 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import Drawer from '@mui/material/Drawer';
-import { Box } from '@mui/material';
+import { Box, Button, Divider, Paper } from '@mui/material';
 import { ReservationDrawerInterface } from '../../interfaces/ReservationDrawerInterface';
+import './ReservationDrawer.scss'
+import { useNavigate } from 'react-router-dom';
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import axios from '../../api/axios.js';
+import { AuthContext } from '../../context/AuthContext';
 
 function ReservationDrawer(props: ReservationDrawerInterface) {
+
+    const navigate = useNavigate();
+    const axiosPrivate = useAxiosPrivate();
+    const context = React.useContext(AuthContext)
 
     const toggleDrawer = (event: React.KeyboardEvent | React.MouseEvent) => {
         if (event.type === 'keydown' && ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')) {
@@ -13,19 +22,63 @@ function ReservationDrawer(props: ReservationDrawerInterface) {
         props.setIsOpen(false);
     };
 
+    const [activeReservations, setActiveReservation] = useState<any[]>([{
+        ClientId: 0
+    }])
+
+    useEffect(() => {
+        console.log(context?.authState)
+        axiosPrivate.get('reservations/reservationstatus/1').then((response) => {
+            if (response.status !== 204) {
+                setActiveReservation(response.data)
+            }
+        })
+        // axios.get('reservations/reservationstatus/1', {
+        //     headers: { 'Content-Type': 'application/json' },
+        //     withCredentials: true
+        // }).then((response) => {
+        //     if (response.status !== 204) {
+        //         setActiveReservation(response.data)
+        //     }
+        // })
+    }, [])
+
     return (
         <Drawer
             anchor='left'
             open={props.isOpen}
-            onClose={toggleDrawer}>
-            <Box sx={{ width: '250px' }}>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure reiciendis tempora nisi totam eligendi inventore, saepe consequuntur nobis vel? Facere qui porro voluptate accusantium quisquam fugiat explicabo molestiae id deserunt?
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure reiciendis tempora nisi totam eligendi inventore, saepe consequuntur nobis vel? Facere qui porro voluptate accusantium quisquam fugiat explicabo molestiae id deserunt?
-
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure reiciendis tempora nisi totam eligendi inventore, saepe consequuntur nobis vel? Facere qui porro voluptate accusantium quisquam fugiat explicabo molestiae id deserunt?
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure reiciendis tempora nisi totam eligendi inventore, saepe consequuntur nobis vel? Facere qui porro voluptate accusantium quisquam fugiat explicabo molestiae id deserunt?
-            </Box>
-        </Drawer>
+            onClose={toggleDrawer}
+            className='reservation_drawer'>
+            <Paper
+                className='reservation_drawer__content'
+                elevation={3}>
+                <Box className='content__header'>
+                    Rezerwacje
+                </Box>
+                <Box className='content__body'>
+                    <Button
+                        className='content__body__button'
+                        onClick={() => navigate('/reservation')}>
+                        Dodaj rezerwacje
+                    </Button>
+                    <Divider className='content__body__divider'>
+                        Aktywne rezerwacje
+                    </Divider>
+                    {activeReservations.map((reserv, key) =>
+                        <>
+                            <div className='content__body__reservation'>
+                                {/* TODO change to tableNumber */}
+                                Stolik nr: {reserv.TableId} <br />
+                                {/* TODO add phone number */}
+                                Tel klienta: { } <br />
+                                Data: { } <br />
+                            </div>
+                            {key < activeReservations.length - 1 ? <Divider /> : ''}
+                        </>
+                    )}
+                </Box>
+            </Paper>
+        </Drawer >
     )
 }
 
