f1df60ad5d15ce602da53341a06b29ae60fc5a7b
Paweł Ciszewski <kardahim@gmail.com>
2022-12-11 13:05:34 +0100

    finish orderHistory

--- src/components/orderHistory/OrderHistory.tsx
+++ src/components/orderHistory/OrderHistory.tsx
@@ -1,7 +1,9 @@
+import { Divider } from '@mui/material';
 import React, { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../../context/AuthContext';
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import './OrderHistory.scss'
+import dayjs from 'dayjs';
 
 function OrderHistory() {
     const context = useContext(AuthContext);
@@ -13,7 +15,7 @@ function OrderHistory() {
     useEffect(() => {
         axiosPrivate.get(`/orderheaders/client/${context?.authState.id}`).then((response) => {
             if (response.status !== 204) {
-                setMyOrders(response.data)
+                setMyOrders(response.data.reverse())
             }
             else setMyOrders([])
         })
@@ -21,7 +23,26 @@ function OrderHistory() {
 
 
     return (
-        <div>OrderHistory</div>
+        <div className='order_history'>
+            {myOrders.map((order, key) => {
+                return (
+                    <div key={key} className='order_history__row'>
+                        <Divider className='order_history__date' textAlign="left">{dayjs(order.updatedAt).format('DD.MM.YYYY')}</Divider>
+                        {order.OrderDetails.map((details: any, key: any) => {
+                            return (
+                                <div className='history__product'>
+                                    <span className='product__name'>{details.Product.name}</span>
+                                    <div style={{ display: "flex", justifyContent: "space-between", width: "170px" }}>
+                                        <span className='product__size'>{details.Product.size}</span>
+                                        <span className='product__price'>{details.Product.price}zł</span>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
