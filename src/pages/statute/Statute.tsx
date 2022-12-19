dea3c91dddf8f16664f3c49e3489df8e15077887
Paweł Ciszewski <kardahim@gmail.com>
2022-12-19 10:50:30 +0100

    add statute text

--- src/pages/statute/Statute.tsx
+++ src/pages/statute/Statute.tsx
@@ -10,20 +10,31 @@ export default function Statute() {
                     Paragraf 1
                 </h1>
                 <p className='statute__card__body'>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, corrupti minima? Odit, fugiat magni! Laboriosam est, aliquid inventore doloribus sed, ducimus ullam ab sint quae eos ipsam quo similique voluptate?
+                    W kawiarni obowiązuje zakaz wnoszenie własnego jedzenia oraz napojów.
                 </p>
                 <h1 className='statute__card__header'>
                     Paragraf 2
                 </h1>
                 <p className='statute__card__body'>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, corrupti minima? Odit, fugiat magni! Laboriosam est, aliquid inventore doloribus sed, ducimus ullam ab sint quae eos ipsam quo similique voluptate?
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, corrupti minima? Odit, fugiat magni! Laboriosam est, aliquid inventore doloribus sed, ducimus ullam ab sint quae eos ipsam quo similique voluptate?
+                    W kawiarni obowiązuje zakaz palenia wyrobów tytoniowych oraz papierosów elektronicznych.
                 </p>
                 <h1 className='statute__card__header'>
                     Paragraf 3
                 </h1>
                 <p className='statute__card__body'>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, corrupti minima? Odit, fugiat magni! Laboriosam est, aliquid inventore doloribus sed, ducimus ullam ab sint quae eos ipsam quo similique voluptate?
+                    Dzieci powinny być nadzorowane przez cały czas i nie powinno być pozostawiane bez opieki.
+                </p>
+                <h1 className='statute__card__header'>
+                    Paragraf 4
+                </h1>
+                <p className='statute__card__body'>
+                    Głośne lub zakłócające spokój zachowanie jest niedozwolone.
+                </p>
+                <h1 className='statute__card__header'>
+                    Paragraf 5
+                </h1>
+                <p className='statute__card__body'>
+                    Wi-Fi w kawiarni jest dla klientów wyłącznie do użytku osobistego i nie powinny być wykorzystywane do celów niezgodnych z prawem lub niewłaściwych.
                 </p>
             </Paper>
         </Container>
